ed04d3156163ad964f9dd930c5fec13b515c1e79
yungalyx <52767711+yungalyx@users.noreply.github.com>
2020-03-02 21:32:43 -0800

    replaced matplotlib.finance with mplfinance (2020); deprecated. attempting to pull data sp500 data via beautifulsoup4 and webreader

--- src/main.py
+++ src/main.py
@@ -1,18 +1,30 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+import mplfinance as mpf
+import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
 
+from src.setup import scrape_sp500_tickers
+
 style.use('ggplot')
 start = dt.datetime(2000, 2, 18) #my birthday!
-end = dt.datetime(2020, 2, 18) #today!! wow 20 years
+end = dt.datetime.now()
+
 
 df = web.DataReader('TSLA', 'yahoo', start, end) # this is dataframe!
+'''
 print("IPO:")
 print(df.head())
 print("RECENT:")
 print(df.tail())
+'''
+
+scrape_sp500_tickers()
+
+
+
 #df.plot()
 '''
  df.to_csv('tsla')
@@ -28,13 +40,33 @@ Subplotting!
 
 df['100ma'] = df['Adj Close'].rolling(window=100).mean()
 
+#4. resampling daily data
+df_ohlc = df['Adj Close'].resample('10D').ohlc()
+df_volume = df['Volume'].resample('10D').sum()
+
+df_ohlc.reset_index(inplace=True)
+
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+
+print(df_ohlc.head())
+#10D, 10M, 6M, #.mean(), .sum(), .rolling(), .ohlc()
+
+print("HELLO")
+mpf.plot(df, type='candle', style='yahoo', mav=(30, 60), volume=True)
+
+
+#3. plotting
 ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
+ax1.xaxis_date()
+
 
 ax1.plot(df.index, df['Adj Close'])
 ax1.plot(df.index, df['100ma'])
 ax2.bar(df.index, df['Volume'])
 
-plt.show()
+#plt.show()
+
+
 
 
---
+++ src/setup.py
@@ -0,0 +1,28 @@
+import bs4 as bs
+import pickle
+import requests
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
+
+def scrape_sp500_tickers():
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, "lxml")
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker)
+
+    with open("sp500tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+
+    print(tickers)
+    return tickers
+
+
+
+
+
+
