728634555667722f7a88b080c64d36bc0c44eb9e
yungalyx <52767711+yungalyx@users.noreply.github.com>
2020-03-09 23:39:28 -0700

    implemented and plotted 30-60 sma trading signals on mpl.plot with new 2020 mpl_finance library... further microadjustments needed due to lack of tutorials online

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@ import mplfinance as mpf
 import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
+import numpy as np
 
 style.use('ggplot')
-start = dt.datetime(2019, 2, 18) #my birthday!
-end = dt.datetime.now()
-
-df = web.DataReader('TSLA', 'yahoo', start, end) # this is dataframe!
+start = dt.datetime(2019, 2, 18)  # my birthday!
+# end = dt.datetime.now()
+end = dt.datetime(2020, 1, 7)
+df = web.DataReader('WM', 'yahoo', start, end)  # this is dataframe!
 
 '''
 print("IPO:")
@@ -19,12 +20,11 @@ print("RECENT:")
 print(df.tail())
 '''
 
-#scrape_sp500_tickers()
-#get_data_from_yahoo()
+# scrape_sp500_tickers()
+# get_data_from_yahoo()
 
 
-
-#df.plot()
+# df.plot()
 '''
  df.to_csv('tsla')
  df = pd.read_csv('tsla', parse_dates=True, index_col=0)
@@ -37,35 +37,55 @@ df.dropna(inplace=True), basically disregards all days where values aren't avail
 Subplotting!
 '''
 
-df['100ma'] = df['Adj Close'].rolling(window=100).mean()
 
-#4. resampling daily data
-df_ohlc = df['Adj Close'].resample('10D').ohlc()
-df_volume = df['Volume'].resample('10D').sum()
-
-df_ohlc.reset_index(inplace=True)
-
-df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
-
-print(df_ohlc.head())
-#10D, 10M, 6M, #.mean(), .sum(), .rolling(), .ohlc()
-
-print("HELLO")
-mpf.plot(df, type='candle', style='yahoo', mav=(30, 60), volume=True)
+# 4. resampling daily data
 
 
-#3. plotting
+# df_ohlc = df['Adj Close'].resample('10D').ohlc()
+# df_volume = df['Volume'].resample('10D').sum()
+# df_ohlc.reset_index(inplace=True)
+
+# df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+# 10D, 10M, 6M, #.mean(), .sum(), .rolling(), .ohlc()
+
+
+def trading_signals(position):
+    grace = position.replace(0, np.nan)
+    return grace
+
+
+# altering dateframe object, notice that mpf.plot and df are two seperate libraries!!
+df['30ma'] = df['Close'].rolling(30).mean()
+df['60ma'] = df['Close'].rolling(60).mean()
+df['signals'] = 0.0
+df['signals'] = np.where(df['30ma'] > df['60ma'], 1.0, 0.0)
+df['pos'] = df['signals'].diff()
+
+# creating a signal dataseries where only correct values are plotted.
+signal = trading_signals(df['pos'])
+
+# creating a scatter plot
+adp = mpf.make_addplot(signal, scatter=True)
+
+# adding our own scatter plot onto the mpf plot
+mpf.plot(df, addplot=adp, type='candle', style='yahoo', mav=(30, 60), volume=True)
+
+print(df[['30ma', '60ma', 'Close', 'signals', 'pos']].tail(10))
+'''
+if df.tail['30ma'] > df.tail['60ma']:
+     print("30 day ma is greater than 60 day ma")
+'''
+
+# 3. plotting
+'''
 ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
 ax1.xaxis_date()
 
 
 ax1.plot(df.index, df['Adj Close'])
-ax1.plot(df.index, df['100ma'])
+ax1.plot(df.index, df['80ma'])
 ax2.bar(df.index, df['Volume'])
 
-#plt.show()
-
-
-
-
+plt.show()
+'''
