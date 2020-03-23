868226118232da4453dac6da56d5d209a29e57ff
yungalyx <52767711+yungalyx@users.noreply.github.com>
2020-03-23 00:55:56 -0700

    QUARANTINE COMMIT 2: properly implemented sell order, positions are closed before a new one is open,

--- src/main.py
+++ src/main.py
@@ -8,30 +8,32 @@ import pandas_datareader.data as web
 import numpy as np
 import backtrader as bt
 
-from src.strategy import sma_plot, TestStrategy
+from src.strategy import sma_plot, TestStrategy, TestStrategy1
 
 # 1.creating pandas dataframe object from web reader
 style.use('ggplot')
 start = dt.datetime(2017, 1, 1)
 end = dt.datetime.now()
-df = web.DataReader('GOOGL', 'yahoo', start, end)
+df = web.DataReader('TSLA', 'yahoo', start, end) # [EDIT]
 
 # 2.reordering dataframe columns, and converting it into CSV for backtrader cerebro
 cols = ['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
 df1 = df.reindex(columns=cols)
 print(df1)
-df1.to_csv("goog.csv")
+df1.to_csv("tsla.csv") # [EDIT]
 
 # 3.attaching csv and strategy to backtrader cerebro
 cerebro = bt.Cerebro()
-data = bt.feeds.YahooFinanceCSVData(dataname='goog.csv')
+data = bt.feeds.YahooFinanceCSVData(dataname='tsla.csv') # [EDIT]
 cerebro.adddata(data)
-cerebro.addstrategy(TestStrategy)
+cerebro.addstrategy(TestStrategy1)
+cerebro.addsizer(bt.sizers.FixedSize, stake=100)
 
 # 4.
 cerebro.broker.set_cash(100000)
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 cerebro.run()
 print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+cerebro.plot()
 
 sma_plot(df, 50, 200)
--- src/strategy.py
+++ src/strategy.py
@@ -7,8 +7,6 @@ import pandas as pd
 import backtrader as bt
 
 
-# given a stock df
-# return plot for sma crossover
 class TestStrategy(bt.Strategy):
 
     def log(self, txt, dt=None):
@@ -35,9 +33,61 @@ class TestStrategy(bt.Strategy):
                 self.buy()
 
 
+class TestStrategy1(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+        # creating an order status
+        self.order = None
+        self.bar_executed = None
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log('BUY EXECUTED {}'.format(order.executed.price))
+            elif order.issell():
+                self.log('SELL EXECUTED {}'.format(order.executed.price))
+
+            self.bar_executed = len(self)
+
+        self.order = None
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        if self.order:
+            return
+
+        if not self.position:
+            if self.dataclose[0] < self.dataclose[-1]:
+                # current close less than previous close
+
+                if self.dataclose[-1] < self.dataclose[-2]:
+                    # previous close less than the previous close
+
+                    # BUY, BUY, BUY!!! (with all possible default parameters)
+                    self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                    self.order = self.buy()
+        else:
+            if len(self) >= (self.bar_executed + 5):
+                self.log('SELL ORDER CREATED {}'.format(self.dataclose[0]))
+                # buy and sell orders are created at end of day, so trades aren't actualized until next day market open
+                self.order = self.sell()
+
+
 def trading_signals(position):
     grace = position.replace(0, np.nan)
     return grace
+
 
 # takes in a yahoo webReader dateFrame object
 def sma_plot(df, short, long):
