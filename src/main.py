3588efe82e5e0a50d720296a065cf8e306c80785
yungalyx <52767711+yungalyx@users.noreply.github.com>
2020-04-10 21:14:40 -0700

    QUARANTINE COMMIT 7: Created simple SMA trend to guard against trading down-trending stocks, usually I'd manually pick stocks that I am familiar with (solid fundamentals) so my results may have been skewed due to survivorship bias. - although this guard successfully prevents trading a downtrend, it also limits potential profits.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from matplotlib import style
 import pandas_datareader.data as web
 import backtrader as bt
 import matplotlib.pyplot as plt
+import pyfolio as pf
 
 from src.strategies.beginner import sma_plot, TestStrategy, TestStrategy1, MAVGStrategy
 from src.strategies.crossover import GoldenCrossStrategy
@@ -11,9 +12,10 @@ from src.strategies.intermediate import AllocationStrategy
 # 1.creating pandas dataframe object from web reader
 style.use('ggplot')
 start = dt.datetime(2017, 1, 1)
+# end = dt.datetime(2020, 1, 1)
 end = dt.datetime.now()
 
-ticker_list = ['GOOGL', 'DAL', 'TSLA', 'WM', 'PG']
+ticker_list = ['PG', 'JCP', 'TSLA', 'RACE'] # Nflx, zm, wm, lulu, LMT,
 
 cols = ['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
 
@@ -35,12 +37,12 @@ df3 = df3.reindex(columns=['High', 'Close', 'Adj Close'])
 print(df3.tail(5))
 
 cerebro.addstrategy(AllocationStrategy)  # [EDIT]
-cerebro.addsizer(bt.sizers.FixedSize, stake=100)
+# cerebro.addsizer(bt.sizers.FixedSize, stake=10)
 
 # 4.
 cerebro.broker.set_cash(100000)
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-cerebro.run()
+strats = cerebro.run()
 print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
 cerebro.plot()
 
--- src/strategies/intermediate.py
+++ src/strategies/intermediate.py
@@ -10,17 +10,23 @@ import math
 
 
 class AllocationStrategy(bt.Strategy):
-    params = (("BBandsPeriod", 20), ("Order_Percentage", 0.5))
+    params = (("BBandsPeriod", 20), ("Order_Percentage", 0.05))
 
     def log(self):
         pass
 
     def __init__(self):
         self.inds = dict()  # going to store indicators in dictionary for each stock ive added
+        self.sma = dict()
         for i, d in enumerate(self.datas):
             self.inds[d] = btind.BollingerBands(d, period=self.params.BBandsPeriod)
-            # self.Orders = none
 
+
+    def market_uptrending(self, d):
+        if self.inds[d].lines.mid[-10] < self.inds[d].lines.mid[-5]:
+            return True
+        else:
+            return False
 
     def next(self):
         cash_on_hand = self.broker.get_cash()
@@ -29,46 +35,20 @@ class AllocationStrategy(bt.Strategy):
 
         for i, d in enumerate(self.datas):  # i refers to index, d refers to data object it is attached to
             dn = d._name
-            print(dn)
 
+            # if the market is uptrending, we can buy dips
+            if self.market_uptrending(d):
             # if close was below botlinger today and yesterday, enter trade
-            if self.datas[i].close[0] < self.inds[d].lines.bot[0]:
-                if self.datas[i].close[-1] < self.inds[d].lines.bot[-1]:
-                    self.size = math.floor(invest_amt / self.data.close)
-                    print("Buy {} shares of {} at {}".format(self.size, dn, self.data.close[0]))
-                    self.buy(data=d, size=self.size)
+                if self.datas[i].close[0] < self.inds[d].lines.bot[0]:
+                    if self.datas[i].close[-1] < self.inds[d].lines.bot[-1]:
+                        self.size = math.floor(invest_amt / self.datas[i].close)
+                        print("LONG: {} shares of {} at {}".format(self.size, dn, self.datas[i].close[0]))
+                        self.buy(data=d, size=self.size)
 
-            elif self.datas[i].close[0] > self.inds[d].lines.top[0]:
-                if self.datas[i].close[-1] > self.inds[d].lines.top[-1]:
-                    print(
-                        "Sell {} shares of {} at {}".format(self.position.size, dn, self.data.close[0]))
-                    self.close(data=d)
-                    # self.order = None
+                elif self.datas[i].close[0] > self.inds[d].lines.top[0]:
+                    if self.datas[i].close[-1] > self.inds[d].lines.top[-1]:
+                        print("CLOSE: {} shares of {} at {}".format(self.position.size, dn, self.datas[i].close[0]))
+                        self.close(data=d)
 
-
-
-
-
-
-
-
-        # if d[0].close < self.inds[d].bot[0]:
-
-            # and yesterday's close was below bband
-            '''
-          
-            if self.datas[-1].close < self.bbands.lines.bot[-1]:
-                self.size = math.floor(invest_amt / self.data.close)
-                print("Buy {} shares of {} at {}".format(self.size, self.params.Ticker, self.data.close[0]))
-                self.buy(size=self.size)
-
-
-        if self.datas[0].close > self.bbands.lines.top[0]:
-            if self.datas[-1].close > self.bbands.lines.top[-1]:
-                print("Sell {} shares of {} at {}".format(self.position.size, self.params.Ticker, self.data.close[0]))
-                self.close()
-                # self.order = None
-  '''
-
-
+            
 
