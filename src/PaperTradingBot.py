89cfd6768162b8f80ab42ce1e3af650a12aa486a
yungalyx <52767711+yungalyx@users.noreply.github.com>
2020-04-27 21:21:29 -0700

    QUARANTINE COMMIT 9: needed data to stream from sources other than alpaca for intraday.

--- src/PaperTradingBot.py
+++ src/PaperTradingBot.py
@@ -1,10 +1,34 @@
-
 import requests, json
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api import StreamConn
 from src.setup import ENDPOINT_URL, SECRET_KEY, API_KEY
 
 ACCOUNT_URL = '{}/v2/account'.format(ENDPOINT_URL)
 ORDER_URL = '{}/v2/orders'.format(ENDPOINT_URL)
 HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+
+class PaperTradingBot:
+
+    def __init__(self):
+        self.alpaca = tradeapi.REST(API_KEY, SECRET_KEY, ENDPOINT_URL, api_version='v2')
+
+    def run(self):
+        # on each minute
+        async def on_minute(conn, channel, bar):
+            if bar.close >= bar.open and bar.open - bar.low > 0.1:
+                print("Buying on Doji Candle!")
+                self.alpaca.submit_order("MSFT", 1, 'buy', 'market', 'day')
+        self.conn = StreamConn('Polygon Key', 'Polygon Key', 'wss://alpaca.socket.polygon.io/stocks')
+        on_minute = self.conn('r' )
+
+            # TODO: create a take profit function at 1% more
+
+
+
+'''
+bd = PaperTradingBot()
+bd.run
+'''
 
 def get_account():
     r = requests.get(ACCOUNT_URL, headers=HEADERS)
